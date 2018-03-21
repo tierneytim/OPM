53c931158b1d02bda6ce791ccdc2fe39e2e0c3d7
Tim Tierney <timtierney1991@gmail.com>
2018-03-21 15:09:16 +0000

    updated help

--- spm_opm_read_lvm.m
+++ spm_opm_read_lvm.m
@@ -2,15 +2,16 @@ function [lbv] = spm_opm_read_lvm(S)
 % FORMAT [lbv] = spm_opm_read_lvm(S)
 %   S               - input structure
 % Optional fields of S:
-%   S.filename      - filepath to LVM file             -Default: no Default                      
-%   S.nchannels     - integer specifying how many      -Default: 81
-%                     channels
-%   S.headerlength  - integer specifying how many      -Default: 23
-%                     lines of file are header
-%   S.timeind       - integer specifying which         -Default: 1
-%                     column is time variable
-%   S.triggerInds   - Indices of trigger Channels      -Default: 73:80
-%   S.trigThresh    - Value to threshold triggers at   -Default: Auto
+%   S.filename           - filepath to LVM file             -Default: no Default                      
+%   S.nchannels          - integer specifying how many      -Default: 81
+%                          channels
+%   S.headerlength       - integer specifying how many      -Default: 23
+%                          lines of file are header
+%   S.timeind            - integer specifying which         -Default: 1
+%                          column is time variable
+%   S.decimalTriggerInds - Indices of trigger Channels      -Default: 74:81
+%   S.binaryTriggerInds  - Indices of trigger Channels      -Default: []
+%   S.trigThresh         - Value to threshold triggers at   -Default: Auto
 %   
 % Output: lbv - output Structure
 %  Fields of lbv:
