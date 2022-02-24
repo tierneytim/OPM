95152530499e95d2c91be26e8578370410a9bce7
George O'Neill <g.o'neill@ucl.ac.uk>
2022-02-24 11:57:44 +0000

    Update headmodel masthead

--- spm_opm_headmodel.m
+++ spm_opm_headmodel.m
@@ -1,8 +1,29 @@
 function [D, L] = spm_opm_headmodel(S)
+% Coregister FIL OPM data and option to set up forward model
+% FORMAT D = spm_opm_create(S)
+%   S               - input structure
+% Fields of S:
+%   S.D             - SPM MEEG Object                          - Default: REQUIRED 
+%   S.coordsystem   - coordsystem.json file                    - Default: transform between sensor space and anatomy is identity
+%   S.sMRI          - Filepath to  MRI file                    - Default: no Default
+%   S.template      - Use SPM canonical template               - Default: 0
+%   S.headhape      - .pos file for better template fit        - Default: no Default
+%   S.cortex        - Custom cortical mesh                     - Default: Use inverse normalised cortical mesh
+%   S.scalp         - Custom scalp mesh                        - Default: Use inverse normalised scalp mesh
+%   S.oskull        - Custom outer skull mesh                  - Default: Use inverse normalised outer skull mesh
+%   S.iskull        - Custom inner skull mesh                  - Default: Use inverse normalised inner skull mesh
+%   S.voltype       - Volume conducter Model type              - Default: 'Single Shell'
+%   S.meshres       - mesh resolution(1,2,3)                   - Default: 1
+%   S.lead          - flag to compute lead field               - Default: 0
+% Output:
+%  D           - MEEG object (also written to disk)
+%  L           - Lead field (also written on disk)
+%__________________________________________________________________________
+% Copyright (C) 2018-2022 Wellcome Centre for Human Neuroimaging
 
-% performs all the wonderful things which the headmodel specification job
-% does, but without a whiff of the matlabbatch anywhere near it. Also has
-% some OPM-specific checks and housekeeping.
+% George O'Neill
+% $Id$
+spm('FnBanner', mfilename);
 
 if ~isfield(S,'D'); error('please specify a MEEG object!'); end
 if ~isfield(S,'template');      S.template = 0;             end
