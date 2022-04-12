aae7276d4da1f0989a681efba5cf2bf10618211c
George O'Neill <george.oneill.90@gmail.com>
2022-04-12 11:32:26 +0100

    Update spm_opm_hfc.m

--- spm_opm_hfc.m
+++ spm_opm_hfc.m
@@ -4,7 +4,7 @@ function [mfD,Yinds] = spm_opm_hfc(S)
 %   S               - input structure
 %  fields of S:
 %   S.D             - SPM MEEG object                                - Default: no Default
-%	S.L 			- Spherical harmonic order (1=homogenous field)	 - Default: 1
+%   S.L             - Spherical harmonic order (1=homogenous field)  - Default: 1
 %   S.usebadchans   - logical to correct channels marked as bad      - Default: 0
 %   S.chunkSize     - max memory usage(for large datasets)           - Default 512(MB)
 %   S.badChanThresh - threshold (std) to identify odd channels       - Default 50 (pT)
