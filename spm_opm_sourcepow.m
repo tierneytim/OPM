7ca149bdcd54fb60b803afb3fa033a0dd0ac3cc6
Tim Tierney <fiachmri@gmail.com>
2022-10-13 13:50:58 +0100

    statistical thresholds

--- spm_opm_sourcepow.m
+++ spm_opm_sourcepow.m
@@ -3,10 +3,14 @@ function [T] = spm_opm_sourcepow(S)
 % FORMAT D = spm_opm_sourcepow(S)
 %   S               - input structure
 %  fields of S:
-%   S.D           - epoched, filtred SPM MEEG object   - Default: no Default
-%   S.basewin     - size of window in ms eg [-1500 0]  - Default: no Default              
-%   S.actwin      - size of window in ms eg [0 1500]   - Default: no Default
-%   S.SNR         - SNR of response(used to regualrise) - Default: 5 
+%   S.D           - epoched, filtred SPM MEEG object           - Default: no Default
+%   S.basewin     - size of window in ms eg [-1500 0]          - Default: no Default              
+%   S.actwin      - size of window in ms eg [0 1500]           - Default: no Default
+%   S.weighted    - boolan apply depth weigting to lead fields - Default: 0
+%   S.thresh      - boolean: apply statistical correction      - Default: 1
+%   S.power       - show power increase/decrease/both          - Default: 0 
+%   S.FWHM        - smooth response in brain space(mm)         - Default: 10
+%   S.SNR         - SNR of response(used to regualrise)        - Default: 5 
 % Output:
 %   T              - matrix of T stats
 %__________________________________________________________________________
@@ -24,7 +28,9 @@ if ~isfield(S, 'basewin'),   error('a time window(ms) must be supplied.'); end
 if ~isfield(S, 'actwin'),    error('a time window(ms) must be supplied.'); end
 if ~isfield(S, 'weighted'),  S.weighted =0; end
 if ~isfield(S, 'SNR'),       S.SNR =5; end
-if ~isfield(S, 'bonf'),      S.bonf =0; end
+if ~isfield(S, 'thresh'),    S.thresh =0; end
+if ~isfield(S, 'power'),     S.power =0; end
+if ~isfield(S, 'FWHM'),      S.FWHM =10; end
 
 %- Get source model
 %--------------------------------------------------------------------------
@@ -71,6 +77,10 @@ brainbase2= sqrt(brainbase);%.^(1/4);
 
 %- time window activation
 %--------------------------------------------------------------------------
+[~,Di] = spm_mesh_neighbours(brain,1);
+muNeighbour= mean(mean(Di));
+n= round((S.FWHM/muNeighbour)^2);
+
 T = zeros(size(L,2),size(S.actwin,1));
 con = T;
 for k = 1:size(S.actwin,1)
@@ -88,6 +98,7 @@ for k = 1:size(S.actwin,1)
     
     braininterest2= sqrt(braininterest);%.^(1/4);
     delta = braininterest2-brainbase2;
+    delta = spm_mesh_smooth(brain,delta,n);
     
     Z = mean(delta,2);
     se = std(delta,[],2)/sqrt(size(S.D,3));
@@ -97,13 +108,55 @@ end
 
 maT = max(max(abs(T)));
 Tplot = T;
-Tplot(4336,:)=maT;
-Tplot(4686,:)=-maT;
-if(S.bonf)
-threshold = spm_uc_Bonf(.05,[1,size(S.D,3)],'T',size(L,2),1);
-    Tplot(abs(Tplot)<threshold)=0;
+
+
+
+
+
+%- statistical threhsolding 
+%--------------------------------------------------------------------------
+if(S.power == -1 )
+    rawp = spm_Tcdf(Tplot(:),[size(S.D,3)]);
+    ps = sort(rawp);
+    [u1] = spm_uc_FDR(.05,[1,size(S.D,3)],'T',1,ps);
+    u2 = spm_uc_Bonf(.05,[1,size(S.D,3)],'T',length(ps),1);
+    u= min([u1,u2]);
+    if(S.thresh)
+        Tplot( Tplot> (-u))=0;
+    else
+        Tplot( Tplot>0)=0;
+    end
+end
+if(S.power == 1 )
+    rawp = 1-spm_Tcdf(Tplot(Tplot>0),[size(S.D,3)]);
+    ps = sort(rawp);
+    [u1] = spm_uc_FDR(.05,[1,size(S.D,3)],'T',1,ps);
+    u2 = spm_uc_Bonf(.05,[1,size(S.D,3)],'T',length(ps),1);
+    u= min([u1,u2]);
+    if(S.thresh)
+        Tplot( Tplot<u)=0;
+    else
+        Tplot( Tplot<0)=0;
+    end
+end
+if(S.power ==0)
+    rawp1 = spm_Tcdf(Tplot(Tplot<0),[size(S.D,3)]);
+    rawp2 = 1-spm_Tcdf(Tplot(Tplot>0),[size(S.D,3)]);
+    ps = sort([rawp1; rawp2]);   
+    [u1] = spm_uc_FDR(.05,[1,size(S.D,3)],'T',1,ps);
+    [u2] = spm_uc_Bonf(.05,[1,size(S.D,3)],'T',size(L,2),1);
+    u= min([u1,u2]);
+    
+    if(S.thresh)
+        Tplot(abs(Tplot)<u)=0;
+    end    
 end
 
+
+%- plotting 
+%--------------------------------------------------------------------------
+Tplot(4336,:)=maT;
+Tplot(4686,:)=-maT;
 
 %for i = 1:size(T,2)
 ax= spm_mesh_render(brain);
@@ -113,6 +166,8 @@ ax = spm_mesh_render('ColourBar',ax,'on');
 ax = spm_mesh_render('ColourMap',ax,colormap(flipud(brewermap(64,'RdBu'))));
 ax.colourbar.Subtitle.String = 't-Statistic';
 ax.colourbar.Subtitle.FontSize=16;
+caxis([-maT maT]);
+
 b = uicontrol('Parent',ax.figure,'Style','slider','Position',[81,54,419,23],...
               'value',1, 'min',1, 'max',size(T,2));
 bl3 = uicontrol('Parent',ax.figure,'Style','text','Position',[240,25,100,23],...
