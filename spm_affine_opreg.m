3215ef971b5f251ca65aaabade9d9882a56ea878
Tim Tierney <fiachmri@gmail.com>
2023-06-12 11:06:51 +0100

    more daiss compliant

--- spm_affine_opreg.m
+++ spm_affine_opreg.m
@@ -1,6 +1,6 @@
-function [tHelm] = spm_affine_opreg(S)
+function [D] = spm_affine_opreg(S)
 % Read magnetometer data and optionally set up forward model
-% FORMAT tHelm = spm_opm_create(S)
+% FORMAT D = spm_opm_create(S)
 %   S               - input structure
 % Optional fields of S:
 %   S.helmetfile     - filepath/matrix(nchannels x timepoints)  - Default:required
@@ -10,7 +10,6 @@ function [tHelm] = spm_affine_opreg(S)
 %   S.headfid
 %   S.headhelmetfid
 %   S.affine  
-%   S.frontonly
 % Output:
 %  tHelm       - transformed helmet object
 %__________________________________________________________________________
@@ -19,10 +18,8 @@ function [tHelm] = spm_affine_opreg(S)
 % Tim Tierney
 % $Id$
 
-if ~isfield(S, 'sensreg'),               S.sensreg = 0; end
 if ~isfield(S, 'affine'),                S.affine = 1; end
 
-%   S.frontonly
 %-read and convert meshes to mm
 %--------------------------------------------------------------------------
 Native= gifti(S.headfile);
@@ -31,6 +28,7 @@ inmetres = sqrt(sum((max(Native.vertices)-min(Native.vertices)).^2))<10;
 if inmetres
 Native.vertices=Native.vertices*1000;
 end
+
 
 
 helmetInfo = load(S.helmetfile);
@@ -45,35 +43,48 @@ end
 %--------------------------------------------------------------------------
 helm2headhelm = spm_eeg_inv_rigidreg(S.headhelmetref',S.helmetref');
 
+% helm_in_headhelm = spm_mesh_transform(helmet,helm2headhelm);
+% mesh_plot(helm_in_headhelm,headhelmet);
+
 
 %-  head with helmet  --> head
 %--------------------------------------------------------------------------
+
 headhelm2head = spm_eeg_inv_rigidreg(S.headfid',S.headhelmetfid');
+% headhelm_in_head = spm_mesh_transform(headhelmet,headhelm2head);
+% mesh_plot(headhelm_in_head,Native);
 
 %- templates 
 %--------------------------------------------------------------------------
 scalp = gifti(fullfile(spm('dir'), 'canonical','scalp_2562.surf.gii'));
 brain = gifti(fullfile(spm('dir'), 'canonical','cortex_5124.surf.gii'));
+iskull = gifti(fullfile(spm('dir'), 'canonical','iskull_2562.surf.gii'));
+oskull = gifti(fullfile(spm('dir'), 'canonical','oskull_2562.surf.gii'));
 
 %- head space --> template (6 paramater)
 %--------------------------------------------------------------------------
-fid_template = spm_eeg_fixpnt(ft_read_headshape(fullfile(spm('dir'), 'EEGtemplates','fiducials.sfp')));
-head2templatescalp = spm_eeg_inv_rigidreg(fid_template.fid.pnt(1:3,:)',S.headfid');
+
+if ~isfield(S, 'templatefid')
+  fid_template = spm_eeg_fixpnt(ft_read_headshape(fullfile(spm('dir'), 'EEGtemplates','fiducials.sfp')));
+  fid_template = fid_template.fid.pnt(1:3,:)';
+else
+  fid_template = S.templatefid;
+end
+
+head2templatescalp = spm_eeg_inv_rigidreg(fid_template,S.headfid');
 scalpTemplate6 = spm_mesh_transform(Native,head2templatescalp);
+mesh_plot(scalpTemplate6,scalp);
 
 %- sensors --> template (6 paramater)
 %--------------------------------------------------------------------------
-sens = helmetInfo.Helmet_info.sens_pos;
-inmetres = sqrt(sum((max(sens)-min(sens)).^2))<10;
-if inmetres
-   sens=sens*1000;
-end
-senstemplate6 = spm_mesh_transform(sens,head2templatescalp*headhelm2head*helm2headhelm);
+sens = S.D.sensors('MEG');
+s = sens.coilpos;
+senstemplate6 = spm_mesh_transform(s,head2templatescalp*headhelm2head*helm2headhelm);
 
 %- cut everything below the ears off of scanned mesh
 %--------------------------------------------------------------------------
 p = double(scalpTemplate6.vertices);
-cut = min(fid_template.fid.pnt(1:3,3));
+cut = min(fid_template(1:3,3));
 p(p(:,3)<cut,:) =[];
 
 %- cut everything  left and right of scalp mesh
@@ -94,20 +105,15 @@ p(cut,:) =[];
 %- affine registration (12 paramater) 
 
 %--------------------------------------------------------------------------
-if S.sensreg
-hmm= spm_eeg_inv_icp(double(scalp.vertices'),senstemplate6',[],[],[],[],S.affine);
-scalpTemplate12 = spm_mesh_transform(scalpTemplate6,hmm);
-else
 hmm= spm_eeg_inv_icp(double(scalp.vertices'),p',[],[],[],[],S.affine);
 scalpTemplate12 = spm_mesh_transform(scalpTemplate6,hmm);
-end
 
 
 %-  sensors to template 
 %--------------------------------------------------------------------------
 sens2temp= hmm*head2templatescalp*headhelm2head*helm2headhelm;
 sens=[];
-sens.vertices =  helmetInfo.Helmet_info.sens_pos*1000;
+sens.vertices =  s;
 senstemplate = spm_mesh_transform(sens,sens2temp);
 helmettemplate = spm_mesh_transform(helmet,sens2temp);
 
@@ -130,9 +136,10 @@ p4.faces = brain.faces;
 p4.vertice = brain.vertices; 
 
 
+
 f = figure()
 
-patch(p1,'FaceColor','red','FaceAlpha',1,'EdgeColor','none')
+patch(p1,'FaceColor','red','FaceAlpha',.4,'EdgeColor','none')
 hold on 
 patch(p2,'FaceColor','blue','FaceAlpha',.3,'EdgeColor','none')
 patch(p3,'FaceColor','green','FaceAlpha',.15,'EdgeColor','none')
@@ -148,15 +155,46 @@ plot3(sensp(:,1),sensp(:,2),sensp(:,3),'.y','MarkerSize',20)
 
 daspect([1,1,1])
 
-%-  save new sensor positions
-%--------------------------------------------------------------------------
-tHelm = helmetInfo.Helmet_info;
-tHelm.sens_pos= senstemplate.vertices;
-Helmet_info = tHelm;
-[a,b,c] = fileparts(S.helmetfile);
-outfile = fullfile(a,['trans_',b,c]);
-save(outfile,'Helmet_info');
+S.D.inv{1}.mesh = spm_eeg_inv_mesh([], 1);
+S.D.inv{1}.mesh.Affine=sens2temp;
 
+%-  registration
+%--------------------------------------------------------------------------
+temp2sens= inv(sens2temp);
+S.D.inv{1}.datareg(1).sensors = S.D.sensors('MEG');
+S.D.inv{1}.datareg(1).toMNI =sens2temp;
+S.D.inv{1}.datareg(1).fromMNI = temp2sens;
+S.D.inv{1}.datareg(1).modality = 'MEG';
+
+%-  save new meshes
+%--------------------------------------------------------------------------
+tbrain = spm_mesh_transform(brain,temp2sens);
+save(tbrain, fullfile(path(S.D),['y_','cortex_5124.surf.gii']));
+S.D.inv{1}.mesh.tess_ctx = fullfile(path(S.D),['y_','cortex_5124.surf.gii']);
+ 
+tscalp = spm_mesh_transform(scalp,temp2sens);
+save(tscalp, fullfile(path(S.D),['y_','scalp_2562.surf.gii']));
+S.D.inv{1}.mesh.tess_scalp  = fullfile(path(S.D),['y_','scalp_2562.surf.gii']);
+
+
+tiskull = spm_mesh_transform(iskull,temp2sens);
+save(tiskull, fullfile(path(S.D),['y_','iskull_2562.surf.gii']));
+S.D.inv{1}.mesh.tess_iskull = fullfile(path(S.D),['y_','iskull_2562.surf.gii']);
+
+toskull = spm_mesh_transform(oskull,temp2sens);
+save(toskull, fullfile(path(S.D),['y_','oskull_2562.surf.gii']));
+S.D.inv{1}.mesh.tess_oskull = fullfile(path(S.D),['y_','oskull_2562.surf.gii']);
+save(S.D)
+
+%-  forward model
+%--------------------------------------------------------------------------
+S.D.inv{1}.forward.voltype = 'Single Shell';
+S.D = spm_eeg_inv_forward(S.D);
+spm_eeg_inv_checkforward(S.D,1,1);
+
+
+D=S.D;
+save(D);
 
 
 end
